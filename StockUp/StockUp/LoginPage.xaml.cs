030b23b90dc7c8db91c79339667a2bce9bd8672c
Xavier La Rosa <xavier.a.larosa@gmail.com>
2020-02-26 02:59:39 -0500

    Added basic Login and Home Page

---
+++ StockUp/StockUp/LoginPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace StockUp
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        void Login_Clicked(System.Object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new HomePage());
+        }
+
+    }
+}
