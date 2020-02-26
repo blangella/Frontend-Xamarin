53faf1d35f636c3ddcc3f4dfb5069476290a15ef
Xavier La Rosa <xavier.a.larosa@gmail.com>
2020-02-26 15:59:00 -0500

    Created basic Admin Home Page
    - will now work on learning data binding

---
+++ StockUp/StockUp/AdminHomePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace StockUp
+{
+    public partial class AdminHomePage : ContentPage
+    {
+        public AdminHomePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
