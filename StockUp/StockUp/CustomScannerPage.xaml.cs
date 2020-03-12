3e35b16f2a903532721b86864cc6013941bba624
Xavier La Rosa <xavier.a.larosa@gmail.com>
2020-03-12 00:36:17 -0400

    Made basics of empty Custom Scanner Page and also presented modally

---
+++ StockUp/StockUp/CustomScannerPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+
+namespace StockUp
+{
+    public partial class CustomScannerPage : ContentPage
+    {
+        public CustomScannerPage()
+        {
+            InitializeComponent();
+            On<iOS>().SetModalPresentationStyle(UIModalPresentationStyle.FormSheet);
+        }
+    }
+}
--- StockUp/StockUp/ScanSummaryPage.xaml.cs
+++ StockUp/StockUp/ScanSummaryPage.xaml.cs
@@ -27,23 +27,45 @@ namespace StockUp
             PacketListView.ItemsSource = tickets;
         }
 
-        private async void Ticket_Tapped(object sender, EventArgs e)
+        async void Ticket_Tapped(object sender, EventArgs e)
         {
             if (((sender as Image).BindingContext is Ticket _selectedTicket))
             {
                 //await DisplayAlert("View Profile", String.Format("{0} \n {1}", _selectedTicket.Id, _selectedTicket.Status), "Okay");
-                scanPage = new ZXingScannerPage ();
-                scanPage.OnScanResult += (result) => {
-                    scanPage.IsScanning = false;
+                //scanPage = new ZXingScannerPage ();
+                //scanPage.OnScanResult += (result) => {
+                //    scanPage.IsScanning = false;
 
-                    Device.BeginInvokeOnMainThread (() => {
-                        Navigation.PopModalAsync ();
-                        DisplayAlert("Scanned Barcode", result.Text, "OK");
-                    });
-                };
+                //    Device.BeginInvokeOnMainThread (() => {
+                //        Navigation.PopModalAsync ();
+                //        DisplayAlert("Scanned Barcode", result.Text, "OK");
+                //    });
+                //};
 
-                await Navigation.PushModalAsync (scanPage);
-                }
+                //await Navigation.PushModalAsync (scanPage);
+
+                await Navigation.PushModalAsync(new CustomScannerPage());
+            }
+        }
+
+        async void Scan_Clicked(System.Object sender, System.EventArgs e)
+        {
+            scanPage = new ZXingScannerPage ();
+            scanPage.OnScanResult += (result) => {
+                scanPage.IsScanning = false;
+
+                Device.BeginInvokeOnMainThread (() => {
+                    Navigation.PopModalAsync ();
+                    DisplayAlert("Scanned Barcode", result.Text, "OK");
+                });
+            };
+
+            await Navigation.PushModalAsync (scanPage);
+        }
+
+        async void Confirm_Clicked(System.Object sender, System.EventArgs e)
+        {
+            await DisplayAlert("Confirm All Scans", "Are you sure you want to confirm?", "OK");
         }
     }
 }
